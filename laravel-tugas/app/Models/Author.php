1869541d41e195e10706cc57cf87f3cc20073499
ASSIDIQ <23040700053@student.umj.ac.id>
2025-10-15 21:21:34 +0700

    Tugas 2 Laravel

--- laravel-tugas/app/Models/Author.php
+++ laravel-tugas/app/Models/Author.php
@@ -7,11 +7,11 @@ class Author
     public static function getAll()
     {
         return [
-            ['id' => 1, 'nama' => 'Tere Liye'],
-            ['id' => 2, 'nama' => 'Andrea Hirata'],
-            ['id' => 3, 'nama' => 'Dewi Lestari'],
-            ['id' => 4, 'nama' => 'Pramoedya Ananta Toer'],
-            ['id' => 5, 'nama' => 'Ahmad Fuadi'],
+            ['id' => 1, 'nama' => 'Tere Liye', 'bio' => ''],
+            ['id' => 2, 'nama' => 'Andrea Hirata', 'bio' => ''],
+            ['id' => 3, 'nama' => 'Dewi Lestari', 'bio' => ''],
+            ['id' => 4, 'nama' => 'Pramoedya Ananta Toer', 'bio' => ''],
+            ['id' => 5, 'nama' => 'Ahmad Fuadi', 'bio' => ''],
         ];
     }
 }
--- laravel-tugas/database/seeders/DatabaseSeeder.php
+++ laravel-tugas/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Author;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Author::factory()
+            ->count(5)
+            ->has(Book::factory()->count(1))
+            ->create();
     }
 }
